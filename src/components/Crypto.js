2d860394a0548bdc68b7d5b54d80b46ceccd0e6f
lustgar <loplust3@gmail.com>
2022-07-22 17:02:59 -0500

    upload archives

---
+++ src/components/Crypto.js
@@ -0,0 +1,64 @@
+import axios from 'axios'
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+
+const Crypto = () => {
+
+const [ search, setSearch] = useState("")
+const [ cryptos, setCryptos] = useState( [] )
+
+const endpoint = 'https://api.coingecko.com/api/v3/coins';
+    
+const showData = () => {
+    axios.get(endpoint).then( (res) => {
+        /* console.log(res.data) */
+        setCryptos(res.data)
+    })
+} 
+
+ useEffect(() => {
+     showData()
+ }, []);
+
+ const searcher = (e) => {
+     setSearch(e.target.value)
+ }
+
+ const results = !search ? cryptos : cryptos.filter( (val)=> val.name.toLowerCase().includes(search.toLocaleLowerCase()) )
+  return (
+      <>
+      <input value={search} onChange={searcher} type='text' placeholder='Search...' className='form-control'/>
+      <table className='table table-dark table-hover mt-3'>
+       <thead>
+           <tr>
+            <th>Ranking</th>
+            <th>Name</th>
+            <th>Symbol</th>
+            <th>Price</th>
+            <th>Price 24h</th>
+        </tr>
+       </thead>
+        <tbody>
+            { results.map( (result) => (
+                <tr key={result.id}>
+                    <td>{result.market_data.market_cap_rank}</td>
+                    <td><small><img src={result.image.small} alt={result.name}/> {result.name} </small></td>
+                    <td>{result.symbol.toUpperCase()}</td>
+                    <td>{result.market_data.current_price.bmd.toFixed(2)}</td>
+                    <td>
+                        {result.market_data.price_change_percentage_24h < 0 ? (
+                            <span className='badge bg-danger'>{result.market_data.price_change_percentage_24h}</span>
+                        ):(
+                            <span className='badge bg-success'>{result.market_data.price_change_percentage_24h}</span>
+                        )}
+                    </td>
+                </tr>
+            ))}
+        </tbody>
+      </table>
+      </>
+  )
+}
+
+export default Crypto
